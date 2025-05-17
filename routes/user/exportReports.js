ffac5555f2fd26f2b5f02cc72bf8e7287efb1b80
Forest Hukill <fahukill@foresthukill.com>
2025-05-17 11:09:18 -0700

    Refactor: Update import statement to use ReportActivity model in exportReports route

--- routes/user/exportReports.js
+++ routes/user/exportReports.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Report = require("../../models/Report");
+const ReportActivity = require("../models/ReportActivity");
 
 const PDFDocument = require("pdfkit");
 const stream = require("stream");
